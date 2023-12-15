e73a36cc96c6a9015f65a6e02b3519a501f198e7
unknown <mhhassan.arif2000@gmail.com>
2023-12-15 11:58:18 -0800

    work complete

---
+++ app.js
@@ -0,0 +1,43 @@
+alert("Muhummad");
+alert("Hasan");
+alert("mhhhassan2000@gmail.com");
+alert("03316496891");
+alert("hasan1234");
+
+
+alert("youre learnigg java script");
+
+alert("i am a student of jawan pakistan");
+
+
+
+var bestman ="hasan";
+alert(bestman);
+
+
+ var a = "caseQTY"
+ var a= "140";
+ 
+ var num ="9";
+
+
+ var merchtotal="100"
+ var shippingcharge="10";
+ var totalorder =merchtotal + shippingcharge;
+ console.log(totalorder);
+
+
+ var productCost=3.45;
+
+ var NameOfBand = CamelCase;
+
+ var hasannumber = 24;
+ var CamelCase =Hasan;
+
+
+ var city = "Karachi"
+ if (city = "Karachi") {
+ console.log("The City OF Lights") 
+}
+
+
